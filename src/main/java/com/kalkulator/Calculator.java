e0e71f77215e1d13a27305b67b26d2d890a62bad
rjagodzki <53404622+rjagodzki@users.noreply.github.com>
2019-09-08 17:28:06 +0200

    Second commit

---
+++ src/main/java/com/kalkulator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kalkulator;
+
+public class Calculator {
+    public void someMethod() {
+        System.out.println("Dziala");
+    }
+    public static void main (String args[]){
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+    }
+}
