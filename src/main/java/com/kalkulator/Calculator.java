d32b5b1bc16ef2592376de21d6343444111874a2
rjagodzki <53404622+rjagodzki@users.noreply.github.com>
2019-09-11 21:30:50 +0200

    cps

--- src/main/java/com/kalkulator/Calculator.java
+++ src/main/java/com/kalkulator/Calculator.java
@@ -1,11 +1,14 @@
 package com.kalkulator;
 
 public class Calculator {
-    public void someMethod() {
-        System.out.println("Dziala");
-    }
+
     public static void main (String args[]){
-        Calculator calculator = new Calculator();
+        class Robie{
+            public void someMethod() {
+                System.out.println("Dziala");
+            }
+        }
+        Robie calculator = new Robie();
         calculator.someMethod();
     }
 }
